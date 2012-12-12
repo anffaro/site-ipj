9c3cfc3535c5ba24da8e58959c8f0c7e2d5718ca
anffaro <anffaro@193bda02-55c9-7941-d3b9-0bbdab821150>
2012-12-12 00:14:29 +0000

    Alteração e inclusão de pastorais

--- Pastoral/index.php
+++ Pastoral/index.php
@@ -19,60 +19,58 @@
     <div id="site_content">
         <!-- insert the page content here -->
         <table border="0" bgcolor="#FFFFFF" width="100%">
-        <tr><td height="33"><center><b><u>Orgulho... De que? </u></b></center></td></tr>
+        <tr><td height="33"><center><b><u>Obrigado... Muito Obrigado!</u></b></center></td></tr>
         <tr><td>
             <p align="justify">&nbsp;&nbsp;&nbsp;
             
-            "Quem subirá ao monte do SENHOR, ou quem estará no seu lugar
-santo? Aquele que é limpo de mãos e puro de coração, que não
-entrega a sua alma à vaidade..." (Salmos 24":3, 4).
+            "Nós o amamos porque ele nos amou primeiro" (1 João 4:19).
 
 <br><br>
             
-            Duas senhoras, em Xangai, estavam conversando sobre Hudson
-Taylor, desejando saber se, alguma vez, ele se sentiu
-tentado a ser orgulhoso. Uma das senhoras se dirigiu à
-esposa de Hudson Taylor e lhe fez a pergunta. A esposa do
-missionário disse que não sabia responder a tal pergunta.
-Mas, depois da conversa, ela foi até o marido e lhe falou
-sobre o assunto. Ele se mostrou surpreso e perguntou:
-"Orgulhoso, de que?" A esposa respondeu: "Pelas coisas que
-você fez". Imediatamente, então, ela recebeu a bela
-resposta: "Que eu saiba, nunca fiz nada". Taylor estava
-certo. Ele nunca fez qualquer coisa. Deus fez tudo nele e
-através dele.
+            
+"Houve, certa vez, um carpinteiro bem treinado que nunca
+pediu salários; que nunca possuiu um lar que pudesse chamar
+de casa própria; um professor jovem e brilhante que nunca
+solicitou aumento de salário; um grande médico que curou
+doentes e aflitos sem nunca perguntar se tinham plano de
+saúde pública ou seguro de saúde particular; um bom vizinho
+que sempre procurou demonstrar seu amor e cortesia. Ele
+viajou por todos os lugares do país e tentou ajudar a todos
+que encontrou pelo caminho; frequentemente alimentava a
+grande quantidade de pessoas que estavam famintas, sem
+perguntar se tinham ou não dinheiro para pagar. Ele estava
+tão envolvido no propósito de ajudar pessoas que chorava
+diante dos que apresentavam uma condição deplorável.
+Contudo, eles o crucificaram, e algumas das muitas pessoas
+que Ele tentou ajudar, juntaram-se à turba insensata que
+zombou dele e cuspiu em seu rosto!"
 
            </p>
             <p align="justify">&nbsp;&nbsp;&nbsp;
-Que grande privilégio é saber que Deus faz grandes e
-maravilhosas coisas, tanto em nossas vidas como através
-delas. Quando isso acontece, nós somos muito abençoados e as
-pessoas que estão junto a nós também o são.
-
-            
-            Quando deixamos Deus trabalhar em nossas vidas, andamos no
-caminho certo, fazemos o que é correto e somos muito mais
-felizes. A nossa vida transmite fé, o nosso rosto mostra um
-sorriso inigualável, nossas atitudes são dignas de serem
-imitadas, os ambientes são iluminados e o nome de Jesus é
-sempre glorificado e engrandecido.
-
+Por que somos assim? Por que não valorizamos o que nos faz
+bem? Por que preferimos o mal, o erro, a mentira, o engano,
+os caminhos incertos e duvidosos? Da mesma forma que Jesus
+amou e abençoou a todos quando esteve nesse mundo, continua
+amando e abençoando a todos que, hoje, abrem o coração para
+Ele, reconhecendo-O como Senhor e Salvador.
             </p>            
             <p align="justify">&nbsp;&nbsp;&nbsp;
            
-De que devemos nos orgulhar? Não de nós mesmos, nem do que
-fazemos, nem dos resultados de nosso trabalho. Nós nos
-orgulhamos pelo fato de termos sido escolhidos, de estarmos
-sendo usados por Deus, de ser um canal de regozijo para
-todos. Nada somos e, ao mesmo tempo, somos tudo. Somos
-filhos de Deus e herdeiros do Céu de glória. Essa é a grande
-razão de nosso contentamento.
+Quando necessitamos de cuidados, Ele continua estendendo as
+mãos para nós. Ele não se preocupava com salários, mas,
+promete suprir todas as nossas necessidades. Ele ensinava às
+multidões em Sua terra e continua sendo o bom Mestre que nos
+ensina o caminho da vitória e da felicidade. Ele ainda é o
+Médico dos médicos que cura as feridas de nossas almas.
 </p>           
             <p align="justify">&nbsp;&nbsp;&nbsp;
-A vaidade é o caminho mais rápido para o fracasso. A
-humildade nos leva muito mais longe... Afinal, ter orgulho
-de que?
-     
+Ele é o meu Deus, o meu Senhor, o meu melhor Amigo, o
+Companheiro que quero levar a todos os lugares para onde
+for. Muitos, no passado, se voltaram contra Ele, porém, eu
+só quero abraçá-lo, dizer que O amo e mostrar toda a minha
+gratidão pelo que fez e continua fazendo por mim.
+
+Obrigado, Senhor... muito obrigado!     
           </td></tr>
           <tr><td><p align="right"><i>Paulo Barbosa<br>Um cego na inernet<br>Fonte: http://ministerio-pararefletir.blogspot.com.br/</i></p></td></tr>
         </table>
---
+++ Pastoral/orgulho_de_que.php
@@ -0,0 +1,85 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <title>Igreja Presbiteriana do Jabaquara</title>
+  <meta name="description" content="website description" />
+  <meta name="keywords" content="website keywords, website keywords" />
+  <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+  <link rel="stylesheet" type="text/css" href="../style/style.css" title="style" />
+</head>
+
+<body>
+  <div id="main">
+        <div>
+  <?php
+  	include($_SERVER['DOCUMENT_ROOT']."/pagina_header.php");
+  ?>
+    </div>
+    <div id="site_content">
+        <!-- insert the page content here -->
+        <table border="0" bgcolor="#FFFFFF" width="100%">
+        <tr><td height="33"><center><b><u>Orgulho... De que? </u></b></center></td></tr>
+        <tr><td>
+            <p align="justify">&nbsp;&nbsp;&nbsp;
+            
+            "Quem subirá ao monte do SENHOR, ou quem estará no seu lugar
+santo? Aquele que é limpo de mãos e puro de coração, que não
+entrega a sua alma à vaidade..." (Salmos 24":3, 4).
+
+<br><br>
+            
+            Duas senhoras, em Xangai, estavam conversando sobre Hudson
+Taylor, desejando saber se, alguma vez, ele se sentiu
+tentado a ser orgulhoso. Uma das senhoras se dirigiu à
+esposa de Hudson Taylor e lhe fez a pergunta. A esposa do
+missionário disse que não sabia responder a tal pergunta.
+Mas, depois da conversa, ela foi até o marido e lhe falou
+sobre o assunto. Ele se mostrou surpreso e perguntou:
+"Orgulhoso, de que?" A esposa respondeu: "Pelas coisas que
+você fez". Imediatamente, então, ela recebeu a bela
+resposta: "Que eu saiba, nunca fiz nada". Taylor estava
+certo. Ele nunca fez qualquer coisa. Deus fez tudo nele e
+através dele.
+
+           </p>
+            <p align="justify">&nbsp;&nbsp;&nbsp;
+Que grande privilégio é saber que Deus faz grandes e
+maravilhosas coisas, tanto em nossas vidas como através
+delas. Quando isso acontece, nós somos muito abençoados e as
+pessoas que estão junto a nós também o são.
+
+            
+            Quando deixamos Deus trabalhar em nossas vidas, andamos no
+caminho certo, fazemos o que é correto e somos muito mais
+felizes. A nossa vida transmite fé, o nosso rosto mostra um
+sorriso inigualável, nossas atitudes são dignas de serem
+imitadas, os ambientes são iluminados e o nome de Jesus é
+sempre glorificado e engrandecido.
+
+            </p>            
+            <p align="justify">&nbsp;&nbsp;&nbsp;
+           
+De que devemos nos orgulhar? Não de nós mesmos, nem do que
+fazemos, nem dos resultados de nosso trabalho. Nós nos
+orgulhamos pelo fato de termos sido escolhidos, de estarmos
+sendo usados por Deus, de ser um canal de regozijo para
+todos. Nada somos e, ao mesmo tempo, somos tudo. Somos
+filhos de Deus e herdeiros do Céu de glória. Essa é a grande
+razão de nosso contentamento.
+</p>           
+            <p align="justify">&nbsp;&nbsp;&nbsp;
+A vaidade é o caminho mais rápido para o fracasso. A
+humildade nos leva muito mais longe... Afinal, ter orgulho
+de que?
+     
+          </td></tr>
+          <tr><td><p align="right"><i>Paulo Barbosa<br>Um cego na inernet<br>Fonte: http://ministerio-pararefletir.blogspot.com.br/</i></p></td></tr>
+        </table>
+    </div>
+    <div id="content_footer"></div>
+    <div id="footer">
+      Copyright &copy; Igreja Presbiteriana do Jabaquara    </div>
+  </div>
+</body>
+</html>
